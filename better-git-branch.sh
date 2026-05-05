4b0b23d7b6b26d5739eb82baa649be5ed4ecaf01
Elvis Dragon Mao <info@elvismao.com>
2026-05-05 16:13:15 +0800

    feat: add remote for remote URL

--- better-git-branch.sh
+++ better-git-branch.sh
@@ -6,11 +6,13 @@ GREEN='\033[0;32m'
 NO_COLOR='\033[0m'
 BLUE='\033[0;34m'
 YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
 
 width1=5
 width2=6
 width3=30 # Width for the current branch
 width4=20
+width5=60
 max_branch_length=$width3
 
 # Get current branch name
@@ -23,15 +25,17 @@ count_commits() {
 }
 
 # Print header
-printf "${GREEN}%-${width1}s ${RED}%-${width2}s ${BLUE}%-${width3}s ${YELLOW}%-${width4}s${NO_COLOR}\n" "Ahead" "Behind" "Branch" "Last Commit"
-printf "${GREEN}%-${width1}s ${RED}%-${width2}s ${BLUE}%-${width3}s ${YELLOW}%-${width4}s${NO_COLOR}\n" "-----" "------" "------------------------------" "-------------------"
+printf "${GREEN}%-${width1}s ${RED}%-${width2}s ${BLUE}%-${width3}s ${YELLOW}%-${width4}s ${CYAN}%-${width5}s${NO_COLOR}\n" "Ahead" "Behind" "Branch" "Last Commit" "Remote URL"
+printf "${GREEN}%-${width1}s ${RED}%-${width2}s ${BLUE}%-${width3}s ${YELLOW}%-${width4}s ${CYAN}%-${width5}s${NO_COLOR}\n" "-----" "------" "------------------------------" "-------------------" "------------------------------------------------------------"
 
-format_string="%(refname:short)@%(committerdate:relative)"
-IFS=$'\n'
+format_string="%(refname:short)%09%(committerdate:relative)%09%(upstream:remotename)"
 
-for line in $(git for-each-ref --sort=-committerdate --format="$format_string" refs/heads/); do
-  branch=$(echo "$line" | cut -d '@' -f1)
-  time=$(echo "$line" | cut -d '@' -f2)
+while IFS=$'\t' read -r branch time remote; do
+  if [ -n "$remote" ]; then
+    remote_url=$(git remote get-url "$remote" 2>/dev/null || printf "-")
+  else
+    remote_url="-"
+  fi
 
   if [ "$branch" = "$current_branch" ]; then
     ahead="-"
@@ -50,5 +54,5 @@ for line in $(git for-each-ref --sort=-committerdate --format="$format_string" r
     short_branch="${branch:0:$((max_branch_length - 1))}…"
   fi
 
-  printf "${GREEN}%-${width1}s ${RED}%-${width2}s ${BLUE}${mark}%-${width3}s ${YELLOW}%-${width4}s${NO_COLOR}\n" "$ahead" "$behind" "$short_branch" "$time"
-done
+  printf "${GREEN}%-${width1}s ${RED}%-${width2}s ${BLUE}${mark}%-${width3}s ${YELLOW}%-${width4}s ${CYAN}%-${width5}s${NO_COLOR}\n" "$ahead" "$behind" "$short_branch" "$time" "$remote_url"
+done < <(git for-each-ref --sort=-committerdate --format="$format_string" refs/heads/)
